067234f57487215014abb03778a0fcc27b6509a5
Andika Kristi <andika.kristi@gmail.com>
2018-11-04 16:59:34 +0800

    Banner ad methods
    - RequestBanner and LoadBanner methods
    - Demo Scene and script

---
+++ Assets/AdsManager/AdDemoScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdDemoScript : MonoBehaviour {
+#if UNITY_ANDROID
+    private string bannerAdUnitId = "ca-app-pub-3940256099942544/6300978111";
+    private string interstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712";
+    private string rewardedVideoAdUnitId = "ca-app-pub-3940256099942544/5224354917";
+    private string nativeAdvancedAdUnitId = "ca-app-pub-3940256099942544/2247696110";
+#elif UNITY_IPHONE
+    private string bannerAdUnitId = "ca-app-pub-3940256099942544/2934735716";
+    private string interstitialAdUnitId = "ca-app-pub-3940256099942544/4411468910";
+    private string rewardedVideoAdUnitId = "ca-app-pub-3940256099942544/1712485313";
+    private string nativeAdvancedAdUnitId = "ca-app-pub-3940256099942544/3986624511";
+#endif
+
+    private AdManager adManager;
+
+    private void Start()
+    {
+        adManager = AdManager.instance;
+    }
+
+    public void ShowBannerAd()
+    {
+        adManager.RequestBanner(bannerAdUnitId, AdSize.SmartBanner, AdPosition.Top);
+        adManager.LoadBanner();
+    }
+
+    public void ShowInterstitialAd()
+    {
+
+    }
+
+}
---
+++ Assets/AdsManager/AdManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdManager : MonoBehaviour {
+    public string appId = "Insert AdMob App ID";
+
+    [Header("Debug Mode")]
+    [Tooltip("Debug mode uses sample add unit ID")]
+    public bool debug = true;
+
+    public static AdManager instance;
+
+    private BannerView bannerView;
+
+    // Singleton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (debug)
+        {
+            // use sample ad unit ID
+#if UNITY_ANDROID
+            appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+            appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+            appId = "unexpected_platform";
+#endif
+        }
+
+        InitializeSDK();
+    }
+
+    private void InitializeSDK()
+    {
+        MobileAds.Initialize(appId);
+    }
+
+    #region banner
+
+    /// <summary>
+    /// <b>Request banner ads.</b>
+    /// </summary>
+    /// <param name="bannerAds"></param>
+    public void RequestBanner(string adUnitId, AdSize adSize, AdPosition adPosition)
+    {
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+
+        bannerView = new BannerView(adUnitId, adSize, adPosition);
+    }
+
+    /// <summary>
+    /// <b>Request banner ads at x, y coordinate.</b>
+    /// <para>
+    /// The top-left corner of the BannerView will be positioned at the x and y values passed to the constructor, where the origin is the top-left of the screen.
+    /// </para>
+    /// </summary>
+    /// <param name="adUnitId"></param>
+    /// <param name="adSize"></param>
+    /// <param name="xCoord"></param>
+    /// <param name="yCoord"></param>
+    public void RequestBanner(string adUnitId, AdSize adSize, int xCoord, int yCoord)
+    {
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+        bannerView = new BannerView(adUnitId, adSize, xCoord, yCoord);
+    }
+
+    /// <summary>
+    /// <b>Load banner ads</b>
+    /// </summary>
+    /// <param name="bannerView"></param>
+    public void LoadBanner()
+    {
+        // do nothing when banner view is null
+        if (bannerView == null) return;
+
+        // create an empty ad request
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+    }
+
+    public void LoadBanner(EventHandler<EventArgs> OnAdLoaded = null,
+        EventHandler<AdFailedToLoadEventArgs> OnAdFailedToLoad = null, EventHandler<EventArgs> OnAdOpening = null,
+        EventHandler<EventArgs> OnAdClosed = null, EventHandler<EventArgs> OnAdLeavingApplication = null)
+    {
+        // do nothing when banner view is null
+        if (bannerView == null) return;
+
+        if (OnAdLoaded != null) bannerView.OnAdLoaded += OnAdLoaded;
+        if (OnAdFailedToLoad != null) bannerView.OnAdFailedToLoad += OnAdFailedToLoad;
+        if (OnAdOpening != null) bannerView.OnAdLoaded += OnAdOpening;
+        if (OnAdClosed != null) bannerView.OnAdLoaded += OnAdClosed;
+        if (OnAdLeavingApplication != null) bannerView.OnAdLoaded += OnAdLeavingApplication;
+
+        LoadBanner();
+    }
+
+    #endregion banner
+}
